56a9b7dcf8fbb893fc8477dafafffe3e1dcf4981
Arafa42 <s107661@ap.be>
2021-01-30 13:09:39 +0100

    spring project added

---
+++ JPAdemo/src/main/java/com/arafa/JPAdemo/JpAdemoApplication.java
@@ -0,0 +1,13 @@
+package com.arafa.JPAdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAdemoApplication.class, args);
+	}
+
+}
---
+++ JPAdemo/src/test/java/com/arafa/JPAdemo/JpAdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arafa.JPAdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
